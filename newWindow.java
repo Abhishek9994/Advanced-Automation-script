a9d9cca8bdc9080d1fe07c9854509bc7fe9611f4
Abhishek9994 <patil.abhishek9994@gamil.com>
2025-02-12 15:40:39 +0530

    new window handeling script

---
+++ newWindow.java
@@ -0,0 +1,43 @@
+package Practice;
+
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class newWindow {
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\patil\\Downloads\\chromedriver-win64 (2)\\chromedriver-win64\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://rahulshettyacademy.com/angularpractice/#");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.switchTo().newWindow(WindowType.TAB);
+		Set<String> WindowHandel = driver.getWindowHandles();
+
+		Iterator<String> it = WindowHandel.iterator();
+		String ParentID = it.next();
+		String ChildID = it.next();
+
+		driver.get("https://rahulshettyacademy.com/");
+		String CourseName = driver
+				.findElement(
+						By.xpath("//a[text()='All-Access Membership-Complete Access to 25+ Courses (and counting!)']"))
+				.getText();
+
+		driver.switchTo().window(ParentID);
+		driver.findElement(By.xpath("//input[@name='name']")).sendKeys(CourseName);
+
+		driver.quit();
+
+	}
+
+}
